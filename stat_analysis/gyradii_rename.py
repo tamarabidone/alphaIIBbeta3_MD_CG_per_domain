c25609b9af66ae5b688f30c9fc167f411b170811
tskora <tskora93@gmail.com>
2024-02-16 10:44:26 -0700

    gyradii file-renaming scripts for statistical analysis

---
+++ stat_analysis/gyradii_rename.py
@@ -0,0 +1,17 @@
+with open('gyradii_analyze_free.txt', 'r') as gyradii_free:
+	original = gyradii_free.read()
+	new = original.replace('Rg_Domain_1_Beta_Propeller_Replica_1', 'Domain_1_rg')\
+				  .replace('Rg_Domain_2_Thigh_Replica_1', 'Domain_2_rg')\
+				  .replace('Rg_Domain_3_Calf_1_Replica_1', 'Domain_3_rg')\
+				  .replace('Rg_Domain_4_Calf_2_Replica_1', 'Domain_4_rg')\
+				  .replace('Rg_Domain_6_Psi_Replica_1', 'Domain_5_rg')\
+				  .replace('Rg_Domain_7_Hybrid_Replica_1', 'Domain_6_rg')\
+				  .replace('Rg_Domain_8_Beta_I_Replica_1', 'Domain_7_rg')\
+				  .replace('Rg_Domain_9_E1_Replica_1', 'Domain_8_rg')\
+				  .replace('Rg_Domain_10_E2_Replica_1', 'Domain_9_rg')\
+				  .replace('Rg_Domain_11_E3_Replica_1', 'Domain_10_rg')\
+				  .replace('Rg_Domain_12_E4_Replica_1', 'Domain_11_rg')\
+				  .replace('Rg_Domain_13_Beta_T_Replica_1', 'Domain_12_rg')
+
+with open('gyradii_analyze_free_renamed.txt', 'w') as gyradii_free:
+	gyradii_free.write(new)
---
+++ stat_analysis/gyradii_rename_2.py
@@ -0,0 +1,25 @@
+with open('gyradii_p_values.txt', 'r') as gyradii:
+	original = gyradii.read().split('\n')
+	if original[-1] == '': original.pop(-1)
+	new = [None for _ in range(13)]
+	for element in original:
+		if element[0] == '#':
+			new[0] = element
+			continue
+		n = int(element.split()[0].split('_')[1])
+		new_element = element.replace('Domain_1_rg', 'Beta_Propeller')\
+					  		 .replace('Domain_2_rg', 'Thigh')\
+					  		 .replace('Domain_3_rg', 'Calf_1')\
+					  		 .replace('Domain_4_rg', 'Calf_2')\
+					  		 .replace('Domain_5_rg', 'Psi')\
+					  		 .replace('Domain_6_rg', 'Hybrid')\
+					  		 .replace('Domain_7_rg', 'Beta_I')\
+					  		 .replace('Domain_8_rg', 'E1')\
+					  		 .replace('Domain_9_rg', 'E2')\
+					  		 .replace('Domain_10_rg', 'E3')\
+					  		 .replace('Domain_11_rg', 'E4')\
+					  		 .replace('Domain_12_rg', 'Beta_T')
+		new[n] = new_element
+
+with open('gyradii_p_values_renamed.txt', 'w') as gyradii:
+	gyradii.write('\n'.join(new))
